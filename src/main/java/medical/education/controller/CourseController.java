d3ba49dfa41296ec9bb34316939453625c8f4a4a
ngohi <ngohieu1811@gmail.com>
2021-03-22 10:14:28 +0700

    thêm cross vào controller

--- src/main/java/medical/education/controller/CourseController.java
+++ src/main/java/medical/education/controller/CourseController.java
@@ -1,12 +1,14 @@
 package medical.education.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.library.controller.BaseController;
 import medical.education.dto.CourseDTO;
 import medical.education.service.CourseService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/courses")
 public class CourseController extends BaseController<CourseDTO, CourseService> {
--- src/main/java/medical/education/controller/SubjectController.java
+++ src/main/java/medical/education/controller/SubjectController.java
@@ -3,10 +3,12 @@ package medical.education.controller;
 import medical.education.dto.SubjectDTO;
 import medical.education.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.library.controller.BaseController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/subjects")
 public class SubjectController extends BaseController<SubjectDTO, SubjectService> {
