1851f820dfa9dc79c18531468cb96f8d8caf1461
ngohi <ngohieu1811@gmail.com>
2021-04-09 15:16:30 +0700

    thêm tính năng load logo

---
+++ src/main/java/medical/education/service/ImageServiceImpl.java
@@ -0,0 +1,56 @@
+package medical.education.service;
+
+import com.google.common.io.ByteStreams;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import spring.backend.library.exception.BaseException;
+
+@Service
+public class ImageServiceImpl implements ImageService{
+  @Value("${image.upload-dir}")
+  private String imagePath;
+
+  private Path imageLocation;
+
+  @Override
+  public ResponseEntity<byte[]> getImage(String fileName,
+      HttpServletRequest httpServletRequest) throws IOException {
+    Resource resource = loadFileAsResource(fileName);
+    final HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.IMAGE_PNG);
+
+    return new ResponseEntity<>(ByteStreams.toByteArray(resource.getInputStream()), headers, HttpStatus.OK);
+  }
+
+  public Resource loadFileAsResource(String fileName) {
+    try {
+      File f = new File(imagePath);
+      if (!f.exists()) {
+        f.mkdirs();
+      }
+
+      imageLocation = Paths.get(imagePath);
+      Path filePath = this.imageLocation.resolve(fileName+".png").normalize();
+      Resource resource = new UrlResource(filePath.toUri());
+      if (resource.exists()) {
+        return resource;
+      } else {
+        throw new BaseException("File not found " + fileName);
+      }
+    } catch (MalformedURLException ex) {
+      throw new BaseException("File not found " + fileName);
+    }
+  }
+}
