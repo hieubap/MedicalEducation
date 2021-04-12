566d67ff61e0b411931b24350c4e6bc83f1d8f43
Tuan Do <tuan2112>
2021-04-12 21:25:45 +0700

    upload

--- src/main/java/medical/education/service/UserServiceImpl.java
+++ src/main/java/medical/education/service/UserServiceImpl.java
@@ -1,13 +1,17 @@
 package medical.education.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import io.jsonwebtoken.Jwts;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 import medical.education.dao.model.RoleEntity;
 import medical.education.dao.model.UserEntity;
 import medical.education.dao.repository.UserRepository;
@@ -16,6 +20,8 @@ import medical.education.dto.UserDTO;
 import medical.education.enums.RoleEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -25,12 +31,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 import spring.backend.library.config.filter.JwtProvider;
 import spring.backend.library.config.filter.JwtProvider.JwtTokenProperties;
 import spring.backend.library.config.userdetail.UserDetail;
 import spring.backend.library.dto.ResponseEntity;
 import spring.backend.library.exception.BaseException;
 import spring.backend.library.service.AbstractBaseService;
+import spring.backend.library.storage.StorageService;
 import spring.backend.library.utils.DigestUtil;
 
 @Service
@@ -45,6 +53,9 @@ public class UserServiceImpl extends
 
   @Autowired
   private JwtProvider jwtProvider;
+
+  @Autowired
+  private StorageService storageService;
 
   @Override
   protected UserRepository getRepository() {
@@ -69,8 +80,8 @@ public class UserServiceImpl extends
     List<String> roles = new ArrayList<>();
 
     String role = "";
-    if(userEntity.getRoleEntity() != null) {
-       role = userEntity.getRoleEntity().getName();
+    if (userEntity.getRoleEntity() != null) {
+      role = userEntity.getRoleEntity().getName();
     }
 
     if (role != null) {
@@ -91,8 +102,7 @@ public class UserServiceImpl extends
   protected void beforeSave(UserEntity entity, UserDTO dto) {
     super.beforeSave(entity, dto);
     if ((dto.getUsername() == null || repository.existsByUsername(dto.getUsername()))
-    && dto.getPasswordChange() == null)
-    {
+        && dto.getPasswordChange() == null) {
       throw new BaseException(400, "username is exists or null");
     }
     if (Strings.isNullOrEmpty(dto.getPassword())) {
@@ -114,8 +124,6 @@ public class UserServiceImpl extends
     }
   }
 
-
-
 //  @Override
 //  protected void specificMapToDTO(UserEntity entity, UserDTO dto) {
 //    super.specificMapToDTO(entity, dto);
@@ -128,8 +136,9 @@ public class UserServiceImpl extends
   @Override
   public ResponseEntity changePassword(UserDTO userDTO) {
     UserDTO user = getCurrentUser();
-    if(!DigestUtil.sha256Hex(userDTO.getPassword()).equals(user.getPassword()))
-      throw new BaseException(400,"password not correct");
+    if (!DigestUtil.sha256Hex(userDTO.getPassword()).equals(user.getPassword())) {
+      throw new BaseException(400, "password not correct");
+    }
 
     user.setPassword(userDTO.getPasswordChange());
     user.setPasswordChange("=))");
@@ -170,7 +179,8 @@ public class UserServiceImpl extends
 
   @Override
   public Long getCurrentUserId() {
-    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+        .currentRequestAttributes()).getRequest();
     String token = request.getHeader("Authorization").replace(preFix, "");
 
     Long userId = Long.valueOf(Jwts.parser()
@@ -184,4 +194,13 @@ public class UserServiceImpl extends
   public Page<UserDTO> search(UserDTO dto, Pageable pageable) {
     return super.search(dto, pageable);
   }
+
+  private final String[] ALLOW_EXTENSIONS = new String[]{"jpg", "png"};
+
+  @Override
+  public String uploadAvatar(MultipartFile file) {
+    storageService.save(file);
+    return "uploads" + "/" + file.getOriginalFilename();
+  }
+
 }
