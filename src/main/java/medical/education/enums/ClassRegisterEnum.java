c2ffb7d32318e1e0fcf2ced38c0e63e7dbd5fbd1
Pham Hoang <hoang.pv@isofh.com>
2021-04-12 17:06:43 +0700

    sửa enum

---
+++ src/main/java/medical/education/enums/ClassRegisterEnum.java
@@ -0,0 +1,22 @@
+package medical.education.enums;
+
+import spring.backend.library.enums.IEnum;
+
+public enum ClassRegisterEnum implements IEnum {
+  THANH_CONG((short) 0),
+  LOP_BI_HUY((short) 1);
+
+  private short value;
+
+  ClassRegisterEnum(short value) {
+    this.value = value;
+  }
+
+  public short getValue() {
+    return value;
+  }
+
+  public void setValue(short value) {
+    this.value = value;
+  }
+}
