09af82a3eea82f4d0c2d3a6a689e5544df6cb6a8
Tuan Do <tuan2112>
2021-06-28 14:03:41 +0700

    fix

--- src/main/java/medical/education/service/ResultServiceImpl.java
+++ src/main/java/medical/education/service/ResultServiceImpl.java
@@ -126,6 +126,7 @@ public class ResultServiceImpl extends
 //    dto.setStudent(userService.findById(entity.getStudentId()));
 //    dto.setCourse(courseService.findById(entity.getCourseId()));
     dto.setSubjectInfo(subjectService.findDetailById(entity.getSubjectId()));
+    dto.setStudentInfo(userService.findById(entity.getId()));
     if(dto.getTotal() != null)
     dto.setRank(applyRank(dto.getTotal()));
   }
