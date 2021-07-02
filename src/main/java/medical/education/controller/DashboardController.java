8945456b3204ad0d3483aeb34268f4132d9a24a0
Tuan Do <tuan2112>
2021-07-02 11:56:06 +0700

    fix

--- src/main/java/medical/education/controller/DashboardController.java
+++ src/main/java/medical/education/controller/DashboardController.java
@@ -3,6 +3,7 @@ package medical.education.controller;
 import medical.education.service.DashboardThongKeSLDKService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import spring.backend.library.controller.BaseResponseController;
 import spring.backend.library.dto.ResponseEntity;
 
+@CrossOrigin(allowedHeaders = "*")
 @RestController
 @RequestMapping("/dashboard")
 public class DashboardController extends BaseResponseController {
